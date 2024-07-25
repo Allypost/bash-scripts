97c7cd768c186fbd81ad15bd2d3bf1a6deaadb5e
Josip Igrec <josip@igr.ec>
2024-07-25 22:13:37 +0200

    Extract out common downloader behaviour

---
+++ python/helpers/download.py
@@ -0,0 +1,226 @@
+import os
+import traceback
+from dataclasses import dataclass, field
+from itertools import chain
+import re
+import subprocess
+from typing import Callable, TypeVar
+from urllib.parse import urlparse
+from python.downloaders import get_download_info
+from python.helpers.list import flatten
+from python.helpers.size import human_byte_size
+from python.log.console import Chalk, Console
+
+
+def download_by_sites(
+    *,
+    download_sites: dict[str, str],
+    episode_url: str,
+    output_file: str,
+    episode_number: int | float,
+    forbidden_cdn_hostnames: list[str] | set[str] = [],
+):
+    Console.log_dim("Trying to find download link...", return_line=True)
+
+    processed = 0
+    for site in download_sites:
+        processed += 1
+        download_url = download_sites[site]
+        try:
+            download_info = get_download_info(download_url, episode_url)
+            if download_info is None:
+                raise NoHandlerException("No handler")
+
+            url = download_info.url
+            referer = download_info.referer or url
+
+            parsed_url = urlparse(url)
+
+            if parsed_url.hostname in forbidden_cdn_hostnames:
+                Console.log(
+                    f"{Chalk.colour(Chalk.italic)}Skipping {site}: {url}{Chalk.colour('23m')}"
+                )
+                continue
+
+            Console.log(
+                f"{Chalk.colour(Chalk.italic)}Downloading from {site}: {url}{Chalk.colour('23m')}"
+            )
+
+            Console.log_dim("Starting download...", return_line=True)
+            download_cmd = [
+                "yt-dlp",
+                "--ignore-config",
+                "--no-warnings",
+                "--no-check-certificate",
+                "--concurrent-fragments",
+                str(os.cpu_count() or 1),
+                "--abort-on-unavailable-fragments",
+                "--retries",
+                "infinite",
+                "--downloader",
+                "ffmpeg",
+                "--downloader-args",
+                "-hide_banner -loglevel error -progress - -nostats",
+                "--referer",
+                referer,
+                *list(
+                    chain(
+                        *[["--add-header", header] for header in download_info.headers]
+                    )
+                ),
+                "--output",
+                output_file,
+                url,
+            ]
+            _download_line_native = re.compile(r"\[download\]\s+(\d+\.\d+%.*)")
+
+            with subprocess.Popen(
+                download_cmd,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                bufsize=1,
+                text=True,
+            ) as proc:
+                Console.log_dim("Waiting for download progress...", return_line=True)
+                for line in proc.stdout or []:
+                    # if download_line_native.search(line):
+                    #     status = line.split(" ", maxsplit=1)[1].strip()
+                    #     Console.log_dim(status, return_line=True)
+                    #     continue
+
+                    # Read lines until status stuff comes along
+                    if line[0] == "[":
+                        continue
+                    break
+
+                download_progress = DownloadProgressInfo()
+                while proc.poll() is None:
+                    try:
+                        # Read status block
+                        for line in proc.stdout or []:
+                            if line.startswith("progress="):
+                                break
+                            key, value = line.strip().split("=", maxsplit=1)
+                            download_progress.set(key, value)
+
+                        Console.log_dim(
+                            str(download_progress),
+                            return_line=True,
+                        )
+                    except Exception:
+                        pass
+
+                ecode = proc.wait()
+                if ecode != 0:
+                    if not proc.stderr:
+                        raise Exception(f"Something broke ({ecode})")
+
+                    proc_stderr = "\n  ".join(proc.stderr.readlines())
+
+                    if "'Connection aborted.'" in proc_stderr:
+                        Console.log_dim("Connection aborted. Discarding URL")
+                        continue
+
+                    print(subprocess.list2cmdline(download_cmd))
+                    raise Exception(
+                        f"Something broke ({ecode}):\nSTDERR:\n  {proc_stderr}"
+                    )
+
+            download_info.after_dl(output_file, download_info)
+        except KeyboardInterrupt:
+            continue
+        except Exception as e:
+            if str(e) == "No handler":
+                Console.log_dim(f"No handler for {download_url} on {site}")
+                continue
+
+            print(("\n" + "=" * 32) * 2)
+            print(e)
+            print(("=" * 32 + "\n") * 2)
+
+        for _ in range(processed):
+            Console.clear_line()
+            Console.move_up(1)
+        Console.log_success(f"Episode {episode_number} downloaded")
+        exit()
+
+    Console.clear_line()
+    Console.move_up(processed)
+    Console.log_error(f"Failed to download episode {episode_number}")
+    exit(1)
+
+
+T = TypeVar("T")
+
+
+@dataclass
+class DownloadProgressInfo:
+    _state: dict[str, str] = field(default_factory=dict)
+
+    def set(self, key: str, value: str):
+        self._state[key.strip()] = value.strip()
+
+    def get(self, key: str, default):
+        return self._state.get(key, default)
+
+    def _s(self, key, pad=0, *, default="???") -> str:
+        return self._state.get(key, default).rjust(pad, " ")
+
+    def _s_if(self, key, pad=0, *, default="???"):
+        if key in self._state:
+            return self._s(key=key, pad=pad, default=default)
+        return None
+
+    @staticmethod
+    def _chain_if(initial: T | None, *processors: Callable[[T], T]) -> T | None:
+        fns = flatten(processors)
+        for fn in fns:
+            if initial is None:
+                break
+            initial = fn(initial)
+        return initial
+
+    def _get_output_layout(self):
+        return [
+            {"time": lambda: self._s_if("out_time")},
+            "|",
+            {
+                "fps": lambda: self._s_if("fps", 6),
+                "speed": lambda: self._s_if("speed", 5),
+                "bitrate": lambda: self._s_if("bitrate", 6),
+            },
+            "|",
+            {
+                "size": lambda: self._chain_if(
+                    self._s_if("total_size"),
+                    lambda x: str(x).strip(),
+                    lambda x: human_byte_size(x),
+                )
+            },
+        ]
+
+    def to_str(self):
+        out: list[str] = []
+        for item in self._get_output_layout():
+            match item:
+                case dict():
+                    for name, value in item.items():
+                        match value:
+                            case gen if callable(gen):
+                                res = gen()
+                                if res is not None:
+                                    out.append(f"{name}={res}")
+                            case None:
+                                out.append(f"{name}")
+                            case value:
+                                out.append(f"{name}={value}")
+                case gen if callable(gen):
+                    res = gen()
+                    if res is not None:
+                        out.append(res)
+                case value:
+                    out.append(str(value))
+        return " ".join(out)
+
+    def __str__(self) -> str:
+        return self.to_str()
--- python/helpers/main.py
+++ python/helpers/main.py
@@ -1,12 +1,36 @@
 import argparse
+import atexit
+from dataclasses import dataclass, field
+import json
 import os
+import signal
+import sys
 from typing import Callable, Any
+
+from python.downloaders import sort_download_links
+from python.helpers.download import download_by_sites
+from python.helpers.list import flatten
+from python.log.console import Chalk, Console
+
+
+ParseArgumentsExtend = Callable[[argparse.ArgumentParser], Any]
+
+
+@dataclass
+class DownloadSite:
+    name: str
+    type: str
+    url: str
+    other: dict[str, Any] = field(default_factory=dict)
+
+    def as_dict(self) -> dict:
+        return self.__dict__
 
 
 def parse_arguments(
     *,
     site_name: str,
-    extend: Callable[[argparse.ArgumentParser], Any] | None = None,
+    extend: ParseArgumentsExtend | None = None,
 ):
     parser = argparse.ArgumentParser(
         description=f"Download videos from {site_name}",
@@ -47,6 +71,23 @@ def parse_arguments(
         dest="number_format",
     )
 
+    parser.add_argument(
+        "-t --type",
+        action="append",
+        help="Whether to download the sub or dub",
+        type=str,
+        nargs="?",
+        dest="series_types",
+    )
+
+    parser.add_argument(
+        "--dump-download-sites",
+        help="Dump the list of download sites and exit",
+        required=False,
+        dest="dump_download_sites",
+        action="store_true",
+    )
+
     if extend:
         extend(parser)
 
@@ -62,3 +103,127 @@ def get_episode_number_to_download(argv) -> float:
     ).read()
 
     return float(last_episode_number or 0) + 1
+
+
+def parse_series_types(*, series_types: list[str], allowed: list[str] | set[str]):
+    series_types_raw = flatten([str(x).split(",") for x in list(series_types)])
+    series_types_raw = [str(x).strip() for x in series_types_raw]
+    series_types = [x for x in series_types_raw if x in allowed]
+
+    if not series_types:
+        raise Exception(
+            f"No known series type provided. Got: {", ".join(series_types_raw)}. Allowed: {", ".join(allowed)}"
+        )
+
+    return series_types
+
+
+def hide_cursor_until_exit():
+    def handle_exit(_signum, _frame):
+        Console.show_cursor()
+        Console.clear_line()
+        Console.log("Hard cancelling download.")
+        sys.exit(2)
+
+    Console.hide_cursor()
+    atexit.register(Console.show_cursor)
+    # signal.signal(signal.SIGINT, handle_exit)
+    signal.signal(signal.SIGTERM, handle_exit)
+    signal.signal(signal.SIGHUP, handle_exit)
+    signal.signal(signal.SIGQUIT, handle_exit)
+
+
+@dataclass
+class DownloadSitesCtx:
+    series_name: str
+    episode_number: float
+    series_types: list[str] | set[str]
+
+
+class EpisodeNumberNotFoundException(Exception):
+    pass
+
+
+def downloader_main(
+    *,
+    site_name: str,
+    episode_page_url_fn: Callable[[str], str],
+    download_sites_fn: Callable[[DownloadSitesCtx], list[DownloadSite] | None],
+    allowed_series_types: list[str] | set[str],
+    with_additional_args: ParseArgumentsExtend | None = None,
+):
+    hide_cursor_until_exit()
+
+    argv = parse_arguments(site_name=site_name, extend=with_additional_args)
+
+    series_name = argv.series_name or argv.series
+    number_format = argv.number_format
+    series_types = parse_series_types(
+        series_types=argv.series_types,
+        allowed=allowed_series_types,
+    )
+
+    if series_name is None:
+        raise Exception("No series name")
+
+    episode_number_offset = float(argv.offset)
+    episode_number = get_episode_number_to_download(argv) - episode_number_offset
+
+    offset_str = (
+        f" (offset episode {episode_number + episode_number_offset})"
+        if episode_number_offset
+        else ""
+    )
+
+    Console.log(
+        f"Downloading {Chalk.badge(series_name, Chalk.black, Chalk.bg_yellow_bright)} episode {Chalk.badge(str(episode_number) + offset_str, Chalk.black, Chalk.bg_blue_bright)}"
+    )
+
+    try:
+        download_sites = download_sites_fn(
+            DownloadSitesCtx(
+                series_name=series_name,
+                episode_number=episode_number,
+                series_types=series_types,
+            )
+        )
+    except EpisodeNumberNotFoundException:
+        Console.log_error(f"Episode {episode_number}{offset_str} can't be found")
+        exit(1)
+
+    if not download_sites:
+        Console.log_error(
+            f"Couldn't download {episode_number}: No download servers found"
+        )
+        exit(1)
+
+    sorted_download_site_urls = sort_download_links(
+        download_sites,
+        to_url=lambda x: x.url,
+    )
+    series_types_order = {t: i for i, t in enumerate(series_types)}
+    sorted_download_site_urls = list(
+        sorted(
+            sorted_download_site_urls,
+            key=lambda x: series_types_order.get(x.type, 9999),
+        )
+    )
+
+    if argv.dump_download_sites:
+        Console.clear_line()
+        Console.log(
+            json.dumps(
+                sorted_download_site_urls, default=lambda o: o.__dict__, indent=2
+            )
+        )
+        Console.log(json.dumps(download_sites, default=lambda o: o.__dict__, indent=2))
+        exit(0)
+
+    download_by_sites(
+        download_sites={
+            f"{item.name} {item.type}": item.url for item in sorted_download_site_urls
+        },
+        episode_number=episode_number,
+        episode_url=episode_page_url_fn(series_name),
+        output_file=f"{number_format % (episode_number + episode_number_offset)}.mp4",
+    )
